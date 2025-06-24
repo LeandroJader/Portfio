3f09434c7c9036d174efa68e522c8f43a292520c
LeandroJader <leziin001@gmail.com>
2025-06-24 17:44:37 -0300

    realizada alterações nos projetos do portfólio

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const dadosCardControleMedicamentos = {
-    titulo: 'Gestão de Equipamentos',
-    imagem: './assets/gestao-de-equipamentos.png',
-    gif: 'https://i.imgur.com/SDgwx8m.gif',
-    urlRepositorio: 'https://github.com/academiadoprogramador-fullstack/controle-de-medicamentos-2025',
+    titulo: 'Controle de Bar',
+    imagem: '',
+    gif: '',
+    urlRepositorio: 'Em implementação',
     stack: [
         'https://skillicons.dev/icons?i=html&theme=light',
         'https://skillicons.dev/icons?i=css&theme=light',
         'https://skillicons.dev/icons?i=cs&theme=light',
         'https://skillicons.dev/icons?i=net&theme=light',
     ],
-    descricao: `O sistema de Gestão de Equipamentos controla o estoque de um almoxarifado, com a gestão de equipamentos e o registro de chamados de manutenção que eventualmente ocorrem durante o uso dos equipamentos. Também são inclusas informações sobre os fabricantes dos equipamentos para contato de suporte ou compras.`
+    descricao: `Em implementação`
 };
 
 const dadosCardGestaoEquipamentos = {
@@ -22,7 +22,7 @@ const dadosCardGestaoEquipamentos = {
         'https://skillicons.dev/icons?i=css&theme=light',
         'https://skillicons.dev/icons?i=cs&theme=light',
         'https://skillicons.dev/icons?i=net&theme=light',
-    ], 
+    ],
     descricao: `O sistema de Gestão de Equipamentos controla o estoque de um almoxarifado, com a gestão de equipamentos e o registro de chamados de manutenção que eventualmente ocorrem durante o uso dos equipamentos. Também são inclusas informações sobre os fabricantes dos equipamentos para contato de suporte ou compras.`
 }
 
@@ -84,7 +84,7 @@ function main() {
     btnSelecionarSegundoCard.addEventListener('click', selecionarSegundoCard);
     btnVerProjeto.addEventListener('click', abrirModalProjeto);
 
-    selecionarSegundoCard();
+    selecionarPrimeiroCard();
 };
 
 document.addEventListener('DOMContentLoaded', main);
